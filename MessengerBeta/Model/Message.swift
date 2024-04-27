0942bd9de2397cf82952b6aed1af7acec843adda
Mia <koring.mia@gmail.com>
2024-04-28 00:30:05 +0200

    Fixed git problem with Message.swift

---
+++ MessengerBeta/Model/Message.swift
@@ -0,0 +1,31 @@
+import Foundation
+import SwiftData
+
+@Model
+final class Message: Identifiable{
+    var time: Int
+    var sender: Int
+    var type: String
+    var reply: Reply?
+    @Relationship(deleteRule: .cascade)
+    var attachments = [Attachment]()
+    var text: String
+    var reactions: [Int: String]
+    var background: String
+    var id: UUID = UUID()
+    var messageID: Int
+    var isRead: Bool = true
+    
+    init(time: Int, sender: Int, type: String = "standalone", reply: Reply? = nil, text: String, reactions: [Int : String] = [:], background: String = "default", id: UUID = UUID(), messageID: Int, isRead: Bool = true) {
+        self.time = time
+        self.sender = sender
+        self.type = type
+        self.reply = reply
+        self.text = text
+        self.reactions = reactions
+        self.background = background
+        self.id = id
+        self.messageID = messageID
+        self.isRead = isRead
+    }
+}
